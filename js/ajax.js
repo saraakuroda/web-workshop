ed3a9af8ff77625cb3d7e9bfc09179ff3f21bd5b
Rukkora <irukkora@gmail.com>
2020-07-01 00:45:53 +0900

    client-side-added

---
+++ js/ajax.js
@@ -0,0 +1,22 @@
+$(function () {
+    $("#submit").on("click", function () {
+        $.ajax({
+            url:
+                "http://127.0.0.1:8888/new?user=" +
+                document.getElementById("user").value +
+                "&kami=" +
+                document.getElementById("kami").value +
+                "&naka=" +
+                document.getElementById("naka").value +
+                "&simo=" +
+                document.getElementById("simo").value,
+            type: "GET",
+            success: function (data, dataType) {
+                alert("投稿できました");
+            },
+            error: function () {
+                alert("投稿に失敗しました");
+            },
+        });
+    });
+});
